f0f03b3f8c5b41be1a42c2ed78dd362b790114cb
Simone Romano <simone.romano@noku.io>
2021-03-09 18:32:10 +0100

    ansible fix, add vars

--- node-provisioning/terraform/digitalocean.tf
+++ node-provisioning/terraform/digitalocean.tf
@@ -12,34 +12,33 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-data "digitalocean_ssh_key" "digital_ocean1" {
-  name = "digital_ocean1"
+data "digitalocean_ssh_key" "digital_ocean_0" {
+  name = "digital_ocean_0"
 }
 
 resource "digitalocean_droplet" "barbabietola_node" {
   count = 1
   size = "s-1vcpu-1gb"
   region = "fra1"
-  image = "72401866"
+  image = "debian-10-x64"
   name = "barbabietola-node-${count.index}"
   #ssh_keys = [ "29745930" ]
-  ssh_keys = [ data.digitalocean_ssh_key.digital_ocean1.id ]
+  ssh_keys = [ data.digitalocean_ssh_key.digital_ocean_0.id ]
 
-  # Copy docker compose
-  provisioner "file" {
-    source = "../../eth-node"
-    destination = "/eth-node"
-
-    connection {
-      type     = "ssh"
-      user     = "root"
-      private_key = file(var.pvt_key)
-      host     = self.ipv4_address
-    }
+  connection {
+    type     = "ssh"
+    user     = "root"
+    private_key = file(var.pvt_key)
+    host     = self.ipv4_address
   }
 
+  provisioner "remote-exec" {
+    inline = ["echo 'server reachable!'"]
+  }
+
+  # Run ansible
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' apache-install.yml"    
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key} --extra-vars 'NODE_ID=${var.node_id} JSON_KEY=${var.json_key} HTTP_PORT=8545 WS_PORT=8546' ../ansible/initial.yml"
   }
 }
 
--- node-provisioning/terraform/variables.tf
+++ node-provisioning/terraform/variables.tf
@@ -3,7 +3,22 @@ variable "do_token" {
   description = "DigitalOcean Personal Access Token"
 }
 
+variable "pub_key" {
+  type = string
+  description = "The public ssh key for the digital ocean resources"
+}
+
 variable "pvt_key" {
   type = string
   description = "The private key for the digital ocean resources"
+}
+
+variable "node_id" {
+  type = number
+  description = "The ETH node id"
+}
+
+variable "json_key" {
+  type = string
+  description = "The ETH node json account password"
 }
