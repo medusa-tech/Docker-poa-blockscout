90bc9a28aac174263bf8bff1bb323f9e9a8d5d64
Simone Romano <simone.romano@noku.io>
2021-03-11 19:46:10 +0100

    bootnodes fix

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Docker-poa-blockscout
+
+
+
+## REFERENCES
+
+* https://openethereum.github.io/JSONRPC-parity-module#parity_enode
+
+* https://eth.wiki/en/fundamentals/enode-url-format
--- node-provisioning/terraform/digitalocean.tf
+++ node-provisioning/terraform/digitalocean.tf
@@ -16,12 +16,12 @@ data "digitalocean_ssh_key" "digital_ocean_0" {
   name = "digital_ocean_0"
 }
 
-resource "digitalocean_droplet" "barbabietola_node" {
-  count = 1
+resource "digitalocean_droplet" "barbabietola_master_node" {
+  # count = 1
   size = "s-1vcpu-1gb"
   region = "fra1"
   image = "debian-10-x64" # "72401866"
-  name = "barbabietola-node-${count.index}"
+  name = "barbabietola-master-node"
   #ssh_keys = [ "29745930" ]
   ssh_keys = [ data.digitalocean_ssh_key.digital_ocean_0.id ]
 
@@ -38,13 +38,61 @@ resource "digitalocean_droplet" "barbabietola_node" {
 
   # Run ansible
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3 ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key} --extra-vars 'NODE_ID=${var.node_id} JSON_KEY=${var.json_key} HTTP_PORT=8545 WS_PORT=8546' ../ansible/initial.yml"
+    command = <<EOT
+      ANSIBLE_HOST_KEY_CHECKING=False \
+      ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3 \
+      ansible-playbook \
+        --user root \
+        --inventory '${self.ipv4_address},' \
+        --tags master \
+        --private-key ${var.pvt_key} \
+        --extra-vars 'NODE_ID=0 JSON_KEY=${var.json_key} NODE_PUBLIC_IP=${self.ipv4_address}' \
+        ../ansible/deploy.yml
+      EOT
   }
 }
 
-output "droplet_ip_addresses" {
-  value = {
-    for droplet in digitalocean_droplet.barbabietola_node:
-    droplet.name => droplet.ipv4_address
+resource "digitalocean_droplet" "barbabietola_slave_node" {
+  count = 1
+  size = "s-1vcpu-1gb"
+  region = "fra1"
+  image = "debian-10-x64" # "72401866"
+  name = "barbabietola-slave-node-${count.index}"
+  #ssh_keys = [ "29745930" ]
+  ssh_keys = [ data.digitalocean_ssh_key.digital_ocean_0.id ]
+
+  depends_on = [
+    digitalocean_droplet.barbabietola_master_node
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "root"
+    private_key = file(var.pvt_key)
+    host     = self.ipv4_address
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'server reachable!'"]
+  }
+
+  # Run ansible
+  provisioner "local-exec" {
+    command = <<EOT
+      ANSIBLE_HOST_KEY_CHECKING=False \
+      ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3 \
+      ansible-playbook \
+        --user root \
+        --inventory '${self.ipv4_address},' \
+        --tags slave \
+        --private-key ${var.pvt_key} \
+        --extra-vars 'NODE_ID=${count.index + 1} JSON_KEY=${var.json_key} NODE_PUBLIC_IP=${self.ipv4_address} MASTER_NODE_IP=${digitalocean_droplet.barbabietola_master_node.ipv4_address}' \
+        ../ansible/deploy.yml
+      EOT
   }
 }
+
+# output "master_ip_address" {
+#   description = "Master node IP Address."
+#   value = digitalocean_droplet.barbabietola_master_node.ipv4_address
+# }
